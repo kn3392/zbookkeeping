c66b349e6dd2da3ba4ed5f0efe1cf9af555cad6b
kn3392 <kn339233@gmail.com>
2026-04-29 13:33:13 +0530

    Fix build errors in non-git environment

--- vite.config.ts
+++ vite.config.ts
@@ -68,12 +68,10 @@ export default defineConfig(() => {
     const buildUnixTime = process.env['buildUnixTime'] || '';
     let buildCommitHash = 'unknown';
 
-    if (fs.existsSync(resolve(__dirname, '.git'))) {
-        try {
-            buildCommitHash = git.short();
-        } catch (e) {
-            console.warn('Cannot get git commit hash, use "unknown" instead.');
-        }
+    try {
+        buildCommitHash = git.short();
+    } catch (e) {
+        console.warn('Cannot get git commit hash, use "unknown" instead.');
     }
 
     const options: UserConfig = {
